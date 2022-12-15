9df5c7679934cbbbc49a8d1b4e794ba2a715b358
ivyroot <jacob@draup.xyz>
2022-12-14 16:03:47 -0800

    add admin withdrawal functions

--- src/DraupMembershipERC721.sol
+++ src/DraupMembershipERC721.sol
@@ -4,8 +4,10 @@ pragma solidity ^0.8.17;
 import "solmate/tokens/ERC721.sol";
 
 contract DraupMembershipERC721 is ERC721 {
+
     constructor() ERC721("Draup Membership", "DRAUP") {}
-   string _BaseURI = "test";
+
+    string _BaseURI = "test";
 
     function mint(address to, uint256 tokenId) public {
         _mint(to, tokenId);
@@ -18,6 +20,17 @@ contract DraupMembershipERC721 is ERC721 {
         returns (string memory)
     {
         return _BaseURI;
+    }
+
+
+    function withdrawAll() external {
+        require(address(this).balance > 0, "Zero balance");
+        (bool sent, ) = owner().call{value: address(this).balance}("");
+        require(sent, "Failed to withdraw");
+    }
+
+    function withdrawAllERC20(IERC20 token) external {
+        token.transfer(owner(), token.balanceOf(address(this)));
     }
 }
 
