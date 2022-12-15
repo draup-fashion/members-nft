b5d3b85e187626d57d4ffb6d32fc4a865ba4b186
ivyroot <jacob@draup.xyz>
2022-12-14 16:24:02 -0800

    initial setup of merkle tree allow list for minting

--- src/DraupMembershipERC721.sol
+++ src/DraupMembershipERC721.sol
@@ -4,15 +4,29 @@ pragma solidity ^0.8.17;
 import "openzeppelin-contracts/contracts/access/Ownable.sol";
 import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-contracts/contracts/utils/cryptography/MerkleProof.sol";
 
 contract DraupMembershipERC721 is ERC721, Ownable {
 
     constructor() ERC721("Draup Membership", "DRAUP") {}
 
+    uint256 public nextTokenId;
+    bytes32 public merkleRoot;
     string _BaseURI = "test";
 
-    function mint(address to, uint256 tokenId) public {
-        _mint(to, tokenId);
+    // Mapping to track who used their allowlist spot
+    mapping(address => bool) public claimed;
+
+    function toBytes32(address addr) pure internal returns (bytes32) {
+        return bytes32(uint256(uint160(addr)));
+    }
+
+    function mint(bytes32[] calldata merkleProof) public {
+        require(claimed[msg.sender] == false, "already claimed");
+        claimed[msg.sender] = true;
+        require(MerkleProof.verify(merkleProof, merkleRoot, toBytes32(msg.sender)) == true, "invalid merkle proof");
+        nextTokenId++;
+        _mint(msg.sender, nextTokenId);
     }
 
     function tokenURI(uint256 tokenId)
